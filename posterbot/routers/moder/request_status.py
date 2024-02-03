a00672169ec6b016781f263e975716e179351d21
PirraToZ <pirratoz@gmail.com>
2024-02-04 00:56:39 +0300

    feat(): add cmd "request status"

--- posterbot/routers/moder/request_status.py
+++ posterbot/routers/moder/request_status.py
@@ -1,2 +1,56 @@
-async def request_status_moderator() -> None:
-    ...
+from asyncio import gather as asyncio_gather
+
+from aiogram.types import Message
+from aiogram import Bot
+
+from posterbot.keyboards.dialogue import kb_request_status_moderator
+from posterbot.utils.answers import TextModerator
+from posterbot.configs import BotConfig
+from posterbot.services import (
+    ModerRequestBuilder,
+    ServiceApiSession,
+)
+
+
+def is_owner(user_id: int, owners: list[int]) -> bool:
+    return user_id in owners
+
+
+async def is_moder(user_id: int, api: ServiceApiSession) -> bool:
+    request_data = await api.send(ModerRequestBuilder().get_moder(user_id))
+    return request_data.status == 200
+
+
+async def send_request(
+    message: Message,
+    config: BotConfig,
+    bot: Bot
+) -> None:
+    text = TextModerator.NEW_REQUEST_FOR_MODERATOR_STATUS.format(
+        fullname=message.from_user.full_name,
+        username=message.from_user.username
+    )
+    markup = kb_request_status_moderator(message.from_user.id)
+    
+    await asyncio_gather(*[
+        bot.send_message(chat_id=owner_id, text=text, reply_markup=markup)
+        for owner_id in config.OWNERS
+    ])
+
+    await message.answer(TextModerator.REQUEST_SENDED)
+
+
+async def request_status_moderator(
+    message: Message,
+    config: BotConfig,
+    api: ServiceApiSession,
+    bot: Bot
+) -> None:
+    
+    if is_owner(message.from_user.id, config.OWNERS):
+        return None
+    
+    if await is_moder(message.from_user.id, api):
+        return None
+
+    await send_request(message, config, bot)
