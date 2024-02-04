0d7e9ced7faf8e39493d5cc3b5f17e2472008f4a
PirraToZ <pirratoz@gmail.com>
2024-02-04 18:08:40 +0300

    feat(): add new filters

--- posterbot/utils/filters/moder.py
+++ posterbot/utils/filters/moder.py
@@ -14,38 +14,36 @@ class ModeratorFilter:
     def REQUEST_STATUS_MODERATOR() -> list[MagicFilter]:
         return [
             (F.text == ModeratorTextCommand.REQUEST_STATUS_MODERATOR) &
+            (F.from_user.id.not_in(BotConfig().OWNERS)) &
             (F.chat.type == ChatType.PRIVATE)
         ]
 
     def ACCEPT_STATUS_MODERATOR() -> list[MagicFilter]:
         return [
             (F.data.startswith(ModeratorCallbackData.ACCEPT_STATUS_MODERATOR)) &
-            (F.chat.type == ChatType.PRIVATE)
+            (F.from_user.id.in_(BotConfig().OWNERS))
         ]
 
     def REJECT_STATUS_MODERATOR() -> list[MagicFilter]:
         return [
             (F.data.startswith(ModeratorCallbackData.REJECT_STATUS_MODERATOR)) &
-            (F.chat.type == ChatType.PRIVATE)
+            (F.from_user.id.in_(BotConfig().OWNERS))
         ]
 
     def SHOW_MODERATORS() -> list[MagicFilter]:
         return [
             (F.data.startswith(ModeratorCallbackData.SHOW_MODERATORS)) &
-            (F.from_user.id.in_(BotConfig().OWNERS)) &
-            (F.chat.type == ChatType.PRIVATE)
+            (F.from_user.id.in_(BotConfig().OWNERS))
         ]
 
     def SHOW_MODERATORS_FOR_DEMOTE() -> list[MagicFilter]:
         return [
             (F.data.startswith(ModeratorCallbackData.SHOW_MODERATORS_FOR_DEMOTE)) &
-            (F.from_user.id.in_(BotConfig().OWNERS)) &
-            (F.chat.type == ChatType.PRIVATE)
+            (F.from_user.id.in_(BotConfig().OWNERS)) 
         ]
 
     def DEMOTE_STATUS_MODERATOR() -> list[MagicFilter]:
         return [
             (F.data.startswith(ModeratorCallbackData.DEMOTE_MODERATOR)) &
-            (F.from_user.id.in_(BotConfig().OWNERS)) &
-            (F.chat.type == ChatType.PRIVATE)
+            (F.from_user.id.in_(BotConfig().OWNERS))
         ]
