5b9e052902a9e57f2d4c915b68a3098f875a983e
PirraToZ <pirratoz@gmail.com>
2024-02-04 19:11:02 +0300

    feat(): add cmd "show moderators"

--- posterbot/routers/moder/show_moderators.py
+++ posterbot/routers/moder/show_moderators.py
@@ -1,2 +1,40 @@
-async def show_moderators() -> None:
-    ...
+from aiogram.types import CallbackQuery
+
+from posterbot.services.db.response import ModerManyResponse
+from posterbot.utils.answers import (
+    TextModerator,
+    TextAnswer,
+)
+from posterbot.services import (
+    ModerRequestBuilder,
+    ServiceApiSession,
+)
+
+
+async def get_moders(api: ServiceApiSession) -> ModerManyResponse | None:
+    request_data = await api.send(ModerRequestBuilder().get_moders())
+    if request_data.status == 200:
+        return ModerManyResponse.model_validate(
+            await request_data.json()
+        )
+    return None
+
+
+def get_moders_text(moders: ModerManyResponse) -> str:
+    text = TextModerator.MODERATOR_LIST
+    for moder in moders.moders:
+        text += f"id{moder.id} | {moder.username} | {moder.fullname}\n"
+    return text
+
+
+async def show_moderators(
+    callback: CallbackQuery,
+    api: ServiceApiSession
+) -> None:
+    
+    moders = await get_moders(api)
+
+    text = get_moders_text(moders) if moders else TextAnswer.OOPS
+    
+    await callback.message.answer(text)
+    await callback.message.delete()
