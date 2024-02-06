d689098ee963f132a9e5b1267b8a84417f4e9623
PirraToZ <pirratoz@gmail.com>
2024-02-06 23:59:12 +0300

    feat(): add cmd "show template"

--- posterbot/routers/template/__init__.py
+++ posterbot/routers/template/__init__.py
@@ -4,8 +4,15 @@ __all__ = [
 
 from aiogram import Router
 
-from posterbot.routers.template.set_title import (set_title_fsm, set_title)
+from posterbot.routers.template.fsm_fill_template_start import fsm_fill_template_start
+from posterbot.routers.template.fsm_fill_template_end import fsm_fill_template_end
+from posterbot.routers.template.fsm_set_title_start import fsm_set_title_start
+from posterbot.routers.template.create_template import create_template
 from posterbot.routers.template.clear_template import clear_template
+from posterbot.routers.template.show_template import show_template
+from posterbot.routers.template.fill_template import fill_template
+from posterbot.routers.template.remove_media import remove_media
+from posterbot.routers.template.set_title import set_title
 
 from posterbot.utils.filters import TemplateFilter
 
@@ -14,8 +21,8 @@ router_template = Router(name="template")
 
 
 router_template.callback_query.register(
-    set_title_fsm,
-    *TemplateFilter.SET_TITLE_TEMPLATE_FSM()
+    fsm_set_title_start,
+    *TemplateFilter.FSM_SET_TITLE_START()
 )
 
 router_template.message.register(
@@ -26,4 +33,29 @@ router_template.message.register(
 router_template.callback_query.register(
     clear_template,
     *TemplateFilter.CLEAR_TEMPLATE()
+)
+
+router_template.callback_query.register(
+    fsm_fill_template_start,
+    *TemplateFilter.FSM_FILL_TEMPLATE_START()
+)
+
+router_template.callback_query.register(
+    fsm_fill_template_end,
+    *TemplateFilter.FSM_FILL_TEMPLATE_END()
+)
+
+router_template.message.register(
+    fill_template,
+    *TemplateFilter.FILL_TEMPLATE()
+)
+
+router_template.callback_query.register(
+    remove_media,
+    *TemplateFilter.REMOVE_MEDIA()
+)
+
+router_template.callback_query.register(
+    show_template,
+    *TemplateFilter.SHOW_TEMPLATE()
 )
---
+++ posterbot/routers/template/show_template.py
@@ -0,0 +1,43 @@
+from aiogram.utils.media_group import MediaGroupBuilder
+from aiogram.types import CallbackQuery
+
+from posterbot.utils.commands import TemplateCallbackRegexp
+from posterbot.utils.answers import TextTemplate
+from posterbot.storages import (
+    TemplateStorage,
+    TemplateAction,
+)
+
+
+async def show_template(
+    callback: CallbackQuery,
+    storage_tmp: TemplateStorage
+) -> None:
+
+    storage = TemplateAction(storage_tmp, callback.message.chat.id)
+
+    album_builder = MediaGroupBuilder(
+        caption=storage.template["text"],
+        caption_entities=storage.template["entities"]
+    )
+
+    is_first_attachments = True
+    for media in storage.template["media"]:
+        album_builder.add(
+            type=media["type"],
+            media=media["file_id"],
+            caption_entities=storage.template["entities"] if is_first_attachments else []
+        )
+        is_first_attachments = False
+
+    if storage.template["media"]:
+        await callback.message.answer_media_group(
+            media=album_builder.build()
+        )
+    else:
+        await callback.message.answer(
+            text=storage.template["text"],
+            entities=storage.template["entities"]
+        )
+
+    await callback.answer()
