c652b46619ed6bd4f4c5c81f666a690cc10404eb
PirraToZ <pirratoz@gmail.com>
2024-02-06 23:45:40 +0300

    feat(): add cmd "fill template"

--- posterbot/routers/template/fill_template.py
+++ posterbot/routers/template/fill_template.py
@@ -1,2 +1,70 @@
-async def fill_template() -> None:
-    ...
+from dataclasses import dataclass
+
+from aiogram.enums import InputMediaType
+from aiogram.types import Message
+
+from posterbot.keyboards.dialogue import kb_remove_media_template
+from posterbot.utils.answers import TextTemplate
+from posterbot.models import Media
+from posterbot.storages import (
+    TemplateStorage,
+    TemplateAction,
+)
+
+
+@dataclass
+class MediaInfo:
+    file_id: str
+    type: str
+    text: str
+
+    def dump(self) -> Media:
+        return Media(file_id=self.file_id, type=self.type)
+
+
+def find_media(message: Message) -> MediaInfo:
+    media = MediaInfo("", "", "")
+
+    if message.photo:
+        media = MediaInfo(
+            file_id=message.photo[-1].file_id,
+            type=InputMediaType.PHOTO.value,
+            text=TextTemplate.PHOTO_APPEND
+        )
+    elif message.video:
+        media = MediaInfo(
+            file_id=message.video.file_id,
+            type=InputMediaType.VIDEO.value,
+            text=TextTemplate.VIDEO_APPEND
+        )
+    
+    return media
+
+
+async def fill_template(
+    message: Message,
+    storage_tmp: TemplateStorage
+) -> None:
+    
+    storage = TemplateAction(storage_tmp, message.from_user.id)
+    markup = kb_remove_media_template
+
+    text = message.text or message.caption
+
+    if text:
+        storage.set_text(text)
+        storage.set_entities(message.entities)
+        await message.reply(
+            text=TextTemplate.TEXT_UPDATED,
+            reply_markup=markup(None)
+        )
+
+    media = find_media(message)
+    
+    if media.file_id:
+        uuid=storage.append_media(media.dump())
+
+        await message.reply(
+            text=media.text,
+            reply_markup=markup(uuid)
+        )
